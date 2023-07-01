0b93e4e0e3602155407959cd3932dd9bf17f24a9
Kaushal Patel <kbpatel3@catamount.wcu.edu>
2023-06-30 22:32:21 -0400

    Added variables for html elements

---
+++ script.js
@@ -0,0 +1,9 @@
+const board = document.querySelector("canvas");
+toolButtons = document.querySelectorAll(".tool");
+fillColor = document.querySelector("#fill-color");
+strokeSize = document.querySelector("#size-slider");
+colorButtons = document.querySelectorAll(".colors .option");
+colorPicker = document.querySelector("#color-picker");
+clearBoard = document.querySelector("#clear-board");
+saveBoard = document.querySelector("#save-board");
+context = board.getContext("2d");
